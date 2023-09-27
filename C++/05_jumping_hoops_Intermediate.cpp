b8a640414838c26fdc16151ab6819bcdc3a01d6e
Ashu7131 <ashishlodhi5559@gmail.com>
2023-09-28 03:46:24 +0530

    05_jumping_hoops_Intermediate file fidex!!

--- C++/05_jumping_hoops_Intermediate.cpp
+++ C++/05_jumping_hoops_Intermediate.cpp
@@ -11,19 +11,6 @@ int main() {
     }
     return 0;
 }
-#include <iostream>
-using namespace std;
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-        cout << (n + 1) / 2 << endl;
-    }
-    return 0;
-}
 
 /**********************************************************
  * @INFO
